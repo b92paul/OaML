5e133bdd247c6779d7b1f9bfa34f8b1381fb2fcf
b92paul <b92paul@gmail.com>
2015-12-30 18:12:33 +0800

    line search gradient method finished

--- main.m
+++ main.m
@@ -1,9 +1,16 @@
-C = 0.1;
-e = 0.1;
-eta = 0.001;
+C = 1;
+e = 0.01;
+eta = 0.01;
 
-[y, x] = libsvmread('data/heart_scale');
+%[y, x] = libsvmread('data/heart_scale');
+[y, x] = libsvmread('data/kddb_test');
+y = y*2-1;
+
+fprintf('read file doen.\n');
+fprintf('start training ...\n');
+
 y_diag = spdiags(y ,0,size(y,1),size(y,1));
 yx = y_diag * x;
-best_w = grad(yx, C, e, eta)
+best_w = grad_line(yx, C, e, eta);
+%best_w = grad(yx, C, e, eta);
 ans = sum((y-(2*(myeval(best_w,x)>=0.5)-1))==0)
