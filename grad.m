11d00ab2a3268863145af1e5610b5bcdaa29d2cc
b92paul <b92paul@gmail.com>
2015-12-30 18:11:34 +0800

    add output message in simple gradient method

--- grad.m
+++ grad.m
@@ -1,4 +1,5 @@
 function best_w = grad(yx, C, eps, eta)
+	t_iter = 0;
 	w = sparse(size(yx,2),1);
 	gd_0 = (grad_one(w, yx, C));
 	eps_gd_0_norm = eps * norm(gd_0);
@@ -6,11 +7,15 @@ function best_w = grad(yx, C, eps, eta)
 	w = w - eta * gd_0;
 	gd_norm = 1e10;
 	while(gd_norm > eps_gd_0_norm)
+		t_iter = t_iter + 1;
 		gd = grad_one(w, yx, C);
 		w = w - eta * gd;
 		gd_norm = norm(gd);
-		%fprintf('gd norm %f\n',gd_norm);
+		if(mod(t_iter, 100) == 0)
+			fprintf('iter:%d, gd_norm = %f\n', t_iter, gd_norm);
+		end
 	end
+	fprintf('total iter = %d\n', t_iter);
 	best_w = w;
 end
 function gd = grad_one(w, yx, C)
