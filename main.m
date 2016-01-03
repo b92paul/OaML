965aab27a6ceaf9422c5cbf90ab2b6208b7d8dea
b92paul <b92paul@gmail.com>
2016-01-04 04:03:59 +0800

    newton-CG with line search finished

--- main.m
+++ main.m
@@ -1,6 +1,7 @@
-C = 1;
-e = 0.01;
+C = 0.1;
+eps = 0.01;
 eta = 0.01;
+eps_k = 0.1;
 
 %[y, x] = libsvmread('data/heart_scale');
 [y, x] = libsvmread('data/rcv1_test');
@@ -10,7 +11,8 @@ fprintf('read file done.\n');
 y_diag = spdiags(y ,0,size(y,1),size(y,1));
 yx = y_diag * x;
 fprintf('start training ...\n');
-tic;best_w = grad_line(yx, C, e, eta);toc;
+%tic;best_w = grad_line(yx, C, eps, eta);toc;
+tic;best_w = newton_line(yx, C, x, eps, eta, eps_k);toc;
 %tic;best_w = grad(yx, C, e, eta);toc;
 ans = sum((y-(2*(myeval(best_w,x)>=0.5)-1))==0)
-obj_func(best_w, yx, C)
+v = obj_func(best_w, yx, C)
---
+++ newton_line.m
@@ -0,0 +1,73 @@
+function best_w = newton_line(yX, C, X, eps, eta, eps_k)
+	w = zeros(size(yX,2),1);
+	wTw = 0;
+	yXw = yX*w;
+	sigm_nyXw = 1./(1+exp(-yXw)); %sigmf(-yXw,[1,0]);
+	gd = w + C * transpose((transpose(sigm_nyXw-1))*yX);
+	gd_norm = norm(gd);
+	gd_0_norm = gd_norm;
+	t_iter = 1;
+	while(gd_norm > eps * gd_0_norm)
+		diagD = sigm_nyXw .* (1-sigm_nyXw);
+		s = my_CG(gd, eps_k, X, diagD, C);
+		% line search
+		alpha = 1.0;
+		eta_gdTs = eta * dot(gd,s);
+		
+		sTs = dot(s,s);
+		wTs = dot(w,s);
+		
+		f_k = obj_func_f(wTw, yXw, C);
+		ayXs = yX*s;
+
+		while(1)
+			f_was = obj_func_fast(wTw, wTs, sTs, yXw + ayXs, alpha, C);
+			f_wPgd = f_k + alpha*eta_gdTs;
+			if(f_was < f_wPgd)
+				break;
+			end
+			alpha = alpha * 0.5;
+			ayXs = ayXs * 0.5;
+		end
+		
+		% update w
+		w = w + alpha * s;
+		wTw = dot(w,w);
+		fprintf('iter: %d, gd_norm = %f, alpha = %f, wnorm = %f\n', t_iter, gd_norm, alpha, sqrt(wTw));
+		% for next step
+		yXw = yX*w;
+		sigm_nyXw = 1./(1+exp(-yXw));
+		gd = w + C * transpose((transpose(sigm_nyXw-1))*yX);
+		gd_norm = norm(gd);
+		t_iter = t_iter+1;
+	end
+	best_w = w;
+end
+function s = my_CG(gd, eps_k, X, diagD, C) % gd = d*1, X = l*d, diagD = l*1
+	s = zeros(size(gd, 1),1);
+	r = -gd;
+	d = -gd;
+	gd_norm = norm(gd);
+	r_norm = gd_norm;
+	i = 0;
+	while(r_norm > eps_k*gd_norm )
+		Xd = X*d; % (l*d)*(d*1) = (l*1), bad part ...
+		DXd = diagD .* Xd; % l*1
+		DXd_T = transpose(DXd);
+		a = (r_norm^2) / ( C*dot(Xd,DXd) + dot(d,d));
+		s = s + a * d;
+		r = r - a * (d + C*transpose(DXd_T*X));
+		r_norm_new  = norm(r);
+		beta = (r_norm_new / r_norm)^2;
+		d = r + beta*d;
+		r_norm = r_norm_new;
+		i = i+1;
+	end
+	fprintf('CG iter: %d\n',i);
+end
+function v = obj_func_f(wTw, yXw, C)
+	v = 0.5*wTw + C*sum(log(1+exp(-yXw)));
+end
+function v = obj_func_fast(wTw, wTs, sTs, yXwPayXs, alpha, C)
+	v = 0.5*(wTw + 2*alpha*wTs + (alpha^2)*sTs) + C*sum(log(1+exp(-yXwPayXs)));
+end
